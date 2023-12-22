dd0317313c22359305eabc5357a73835558036ab
PIK4 <8305727+PIK4@users.noreply.github.com>
2023-12-22 16:40:38 +0800

    wip

--- public/new/js/modules/db.js
+++ public/new/js/modules/db.js
@@ -20,89 +20,106 @@ function deferred(request) {
 /**
  * @see https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
  */
-export default class DB {
+export default new Proxy(
+    class DB {
 
-    /**
-     * @type {IDBDatabase}
-     */
-    #db
+        /**
+         * @type {IDBDatabase}
+         */
+        #db
+        /**
+         * @type {EventTarget}
+         */
+        #dispatcher
 
-    #name
-    #version
-    #migration
+        #name
+        #version
+        #migration
 
-    /**
-     * 
-     * @param {String} name database name
-     * @param {Number} version integer
-     * @param {(db: IDBDatabase) => void} migration 
-     */
-    constructor(name, version, migration = db => { }) {
-        this.#name = name
-        this.#version = version
-        this.#migration = migration
-        this.#bootstrap()
-    }
+        /**
+         * 
+         * @param {String} name database name
+         * @param {Number} version integer
+         * @param {(db: IDBDatabase) => void} migration 
+         */
+        constructor(name, version, migration = db => { }) {
+            this.#name = name
+            this.#version = version
+            this.#migration = migration
+            this.#bootstrap()
+        }
 
-    async #bootstrap() {
-        this.#db = await this.#connect({
-            db_name: this.#name,
-            db_version: this.#version,
-            onupgradeneeded: ({ target: { result } }) => this.#migration(result)
-        })
+        async #bootstrap() {
+            this.#db = await this.#connect({
+                db_name: this.#name,
+                db_version: this.#version,
+                onupgradeneeded: ({ target: { result } }) => this.#migration(result)
+            })
 
-        this.store = new Proxy(this.store, {
-            get: (target, property) => Reflect.apply(target, this, [property])
-        })
-    }
+            this.store = new Proxy(this.store, {
+                get: (target, property) => Reflect.apply(target, this, [property])
+            })
 
-    #connect(db_name, db_version = 1, onupgradeneeded = e => { }) {
-        const request = window.indexedDB.open(db_name, db_version)
-        request.addEventListener('upgradeneeded', onupgradeneeded)
-        return deferred(request)
-    }
+            this.#dispatcher = new EventTarget()
+        }
 
-    /**
-     * 
-     * @param {IDBDatabase} db 
-     * @param {String} store_name 
-     * @param {String|'readonly'|'readwrite'} mode 
-     * @returns {IDBTransaction}
-     */
-    getTransaction(store_name, mode = 'readonly') {
-        return this.#db.transaction(store_name, mode)
-    }
+        #connect(db_name, db_version = 1, onupgradeneeded = e => { }) {
+            const request = window.indexedDB.open(db_name, db_version)
+            request.addEventListener('upgradeneeded', onupgradeneeded)
+            return deferred(request)
+        }
 
-    /**
-     * 
-     * @param {String} store_name 
-     * @param {'readonly'|'readwrite'} mode 
-     * @returns {IDBObjectStore}
-     */
-    getStore(store_name, mode = 'readonly') {
-        return this.getTransaction(store_name, mode).objectStore(store_name)
-    }
+        /**
+         * 
+         * @param {IDBDatabase} db 
+         * @param {String} store_name 
+         * @param {String|'readonly'|'readwrite'} mode 
+         * @returns {IDBTransaction}
+         */
+        getTransaction(store_name, mode = 'readonly') {
+            return this.#db.transaction(store_name, mode)
+        }
 
-    /**
-     * 
-     * @param {String} store_name 
-     * @returns {Proxy<IDBObjectStore>}
-     */
-    store(store_name) {
-        return new Proxy(this.getStore(store_name, 'readonly'), {
-            get: (target, property) => {
-                if (typeof target[property] == 'function') {
+        /**
+         * 
+         * @param {String} store_name 
+         * @param {'readonly'|'readwrite'} mode 
+         * @returns {IDBObjectStore}
+         */
+        getStore(store_name, mode = 'readonly') {
+            return this.getTransaction(store_name, mode).objectStore(store_name)
+        }
 
-                    if (['add', 'put', 'delete', 'deleteIndex', 'clear'].includes(property)) {
-                        target = this.getStore(store_name, 'readwrite')
+        /**
+         * 
+         * @param {String} store_name 
+         * @returns {Proxy<IDBObjectStore>}
+         */
+        store(store_name) {
+            return new Proxy(this.getStore(store_name, 'readonly'), {
+                get: (target, property) => {
+                    if (typeof target[property] == 'function') {
+
+                        if (['add', 'put', 'delete', 'deleteIndex', 'clear'].includes(property)) {
+                            target = this.getStore(store_name, 'readwrite')
+                        }
+
+                        this.#dispatcher.dispatchEvent(new CustomEvent(`${store_name}.${property}`, { target }))
+
+                        return (...args) => deferred(Reflect.apply(target[property], target, args))
                     }
-                    
-                    return (...args) => deferred(Reflect.apply(target[property], target, args))
-                }
 
-                return target[property]
+                    return target[property]
+                }
+            }
+            )
+        }
+    },
+    {
+        get(target, property) {
+            if(!Reflect.has(target, property)){
+                return Reflect.apply(target.__call)
             }
         }
-        )
     }
-}
+)
